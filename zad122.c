b3d655ddf7808a889f0eaeb0d022d5ef91de106d
fiki2k <91737444+fiki2k@users.noreply.github.com>
2022-03-24 09:47:51 +0100

    Add files via upload

---
+++ zad122.c
@@ -0,0 +1,85 @@
+// Program treba provjerit jeli matrica gornje trokutasta.
+
+#include <stdio.h>
+
+int main()
+{
+    int A[10][10],i,j,m,n;
+    int redak, stupac, gornje;
+
+    printf("Unesi broj redaka: ");
+        scanf("%d", &m);
+        printf("\nUnesi broj stupaca: ");
+        scanf("%d",&n);
+        printf("\nUnesi vrijednosti matrice \n");
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                 printf("\nUnesi vrijednost A[%d][%d] : ",i,j);
+                 scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("\nDana matrica je : \n\n");
+
+        for (i = 0; i < m; ++i)
+        {
+            for (j = 0; j < n; ++j)
+            {
+                printf("\t%d", A[i][j]);
+            }
+            printf("\n\n");
+        }
+
+    /*
+     Provjer je li matrica gornje trokutasta
+     */
+    gornje = 1;
+    for(redak=0; redak<m; redak++)
+    {
+        for(stupac=0; stupac<n; stupac++)
+        {
+            /*
+             Ako su elementi ispod glavne dijagonale (col<row)
+             * 0 onda je matrica gornje trokutasta.
+             */
+            if(stupac<redak && A[redak][stupac]!=0)
+            {
+                gornje = 0;
+            }
+        }
+    }
+
+    /*
+     * If it is upper triangular matrix
+     */
+    if(gornje==1)
+    {
+        printf("\nOvo je gornje trokutasta matrica.\n");
+
+        for(redak=0; redak<m; redak++)
+        {
+            for(stupac=0; stupac<n; stupac++)
+            {
+                if(A[redak][stupac] != 0)
+                {
+                    printf("\t%d ", A[redak][stupac]);
+                }
+                else
+                {
+                    printf("\t");
+                }
+
+            }
+
+            printf("\n\n");
+        }
+    }
+    else
+    {
+        printf("\nOvo nije gornja trokutasta matrica.");
+    }
+
+    return 0;
+}
